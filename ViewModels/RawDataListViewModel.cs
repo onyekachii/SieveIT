f51040f843da93669eb9562de9b305d2aff59c01
Kachii007 <ugwu_kennedy@yahoo.com>
2025-04-13 16:11:21 +0100

    handle exception when run command is called on bad data

--- ViewModels/RawDataListViewModel.cs
+++ ViewModels/RawDataListViewModel.cs
@@ -70,23 +70,31 @@ namespace SeiveIT.ViewModels
 
         double GetXValueForY(double yValue)
         {
-            var lineSeries = PlotModeler.Series.OfType<LineSeries>().FirstOrDefault();
-            if (lineSeries == null)            
-                throw new InvalidOperationException("No LineSeries found in the plot.");
-            
-            for (int i = 0; i < lineSeries.Points.Count - 1; i++)
+            try
             {
-                var p1 = lineSeries.Points[i];
-                var p2 = lineSeries.Points[i + 1];
+                var lineSeries = PlotModeler.Series.OfType<LineSeries>().FirstOrDefault();
+                if (lineSeries == null)
+                    throw new InvalidOperationException("No LineSeries found in the plot.");
 
-                if ((p1.Y <= yValue && p2.Y >= yValue) || (p1.Y >= yValue && p2.Y <= yValue))
+                for (int i = 0; i < lineSeries.Points.Count - 1; i++)
                 {
-                    double xValue = p1.X + (yValue - p1.Y) * (p2.X - p1.X) / (p2.Y - p1.Y);
-                    return xValue;
-                }
-            }
+                    var p1 = lineSeries.Points[i];
+                    var p2 = lineSeries.Points[i + 1];
 
-            throw new InvalidOperationException("No intersection found for the specified Y value.");
+                    if ((p1.Y <= yValue && p2.Y >= yValue) || (p1.Y >= yValue && p2.Y <= yValue))
+                    {
+                        double xValue = p1.X + (yValue - p1.Y) * (p2.X - p1.X) / (p2.Y - p1.Y);
+                        return xValue;
+                    }
+                }
+
+                throw new InvalidOperationException("No intersection found for the specified Y value.");
+            }
+            catch (Exception ex)
+            {
+                Shell.Current.DisplayAlert("An error occured", ex.Message, "OK");
+                return 0;
+            }
         }
 
         void Load()

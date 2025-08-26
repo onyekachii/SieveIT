25cf78de37c52656214e1d5910457baabc0320e6
Kachii007 <ugwu_kennedy@yahoo.com>
2025-08-26 14:02:03 +0100

    Bug fix: Issue 21.2... replaced toast with popup after project created

--- ViewModels/AddprojectPopupViewModel.cs
+++ ViewModels/AddprojectPopupViewModel.cs
@@ -53,14 +53,13 @@ namespace SeiveIT.ViewModels
                 });
 
                 await Cancel();
-                await Toast.Make("Project saved").Show();
                 await Shell.Current.GoToAsync($"project?id={proj.Id}");
             }
             catch (SQLite.SQLiteException e)
             {
                 var a = e.GetType();
                 string errMsg = e.Message.Contains("UNIQUE constraint failed") ? "Title already exists" : "Unable to Save data, send us a mail";
-                await Toast.Make(errMsg).Show();
+                await Application.Current.MainPage.DisplayAlert("Error", errMsg, "OK");
             }
             catch(Exception e)
             {
